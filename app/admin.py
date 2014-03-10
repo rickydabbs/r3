a2c8e710997a66e1a1bc91cb1b752c2ff4d4db9b
Ricky Dabbs <rickydabbs@gmail.com>
2014-03-10 10:57:28 -0500

    added columns to property admin

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Code, Prop, Tran
 
 # Register your models here.
+
 admin.site.register(Code)
-admin.site.register(Prop)
+
+class PropAdmin(admin.ModelAdmin):
+	list_display = ('short_name', 'address', 'city', 'state', 'zip', 'enis_no', 'mgmt_co', 'mgmt_pct')
+admin.site.register(Prop, PropAdmin)
 
 class TranAdmin(admin.ModelAdmin):
 	list_display = ('prop', 'date', 'code', 'amt')
